ec8248a6a8fea2e80c4207829d3170c326818ca5
Sachith Gunasekara <sachithgunasekara@outlook.com>
2025-03-11 12:04:42 +0530

    Commit to continue on remote VM

--- evals/helpers/llm.py
+++ evals/helpers/llm.py
@@ -1,3 +1,5 @@
+import os
+
 from langchain_core.rate_limiters import InMemoryRateLimiter
 from langchain_sambanova import ChatSambaNovaCloud
 from langchain_mistralai import ChatMistralAI
@@ -9,12 +11,17 @@ from self_discover.helpers.logger import logger
 
 llama = True if "llama" in config["MODEL"]["model_type"] else False
 
-MODEL_ID = "hf:meta-llama/Llama-3.1-405B-Instruct" if llama else "mistral-large-2407"
+
+# MODEL_ID = "hf:meta-llama/Llama-3.1-405B-Instruct" if llama else "mistral-large-2407" # Use if llama model is from glhf.chat
+# MODEL_ID = "Meta-Llama-3.1-405B-Instruct" if llama else "mistral-large-2407" # Use if llama model is from Sambanovacloud
+MODEL_ID = (
+    "meta-llama/Meta-Llama-3.1-405B-Instruct" if llama else "mistral-large-2407"
+)  # Use if llama model is from Hyperbolic
 
 logger.info("Using {} for inference", MODEL_ID)
 
 rate_limiter = InMemoryRateLimiter(
-    requests_per_second=0.5,
+    requests_per_second=0.083,
     check_every_n_seconds=5,
     max_bucket_size=1,
 )
@@ -22,21 +29,35 @@ rate_limiter = InMemoryRateLimiter(
 model_kwargs = {
     "temperature": 0.2,
     "top_p": 0.9,
-    "top_k": 15,
-    "max_tokens": 2048 if llama else 10240,
+    # "top_k": 15,
+    "max_tokens": 8192,
 }
 
 
 if llama:
-    model = ChatSambaNovaCloud(
-        model=MODEL_ID, rate_limiter=rate_limiter, **model_kwargs
-    )
+    # model = ChatSambaNovaCloud(
+    #     model=MODEL_ID, rate_limiter=rate_limiter, **model_kwargs
+    # )
+
+    # model = ChatOpenAI(
+    #     model=MODEL_ID,
+    #     base_url="https://glhf.chat/api/openai/v1",
+    #     rate_limiter=rate_limiter,
+    #     **model_kwargs
+    # )
 
     model = ChatOpenAI(
         model=MODEL_ID,
-        base_url="https://glhf.chat/api/openai/v1",
+        base_url="https://api.hyperbolic.xyz/v1",
+        api_key=os.environ["HYPERBOLIC_API_KEY"],
         rate_limiter=rate_limiter,
         **model_kwargs
     )
+
+    # model = ChatNVIDIA(
+    #     model=MODEL_ID,
+    #     rate_limiter=rate_limiter,
+    #     **model_kwargs
+    # )
 else:
     model = ChatMistralAI(model=MODEL_ID, rate_limiter=rate_limiter, **model_kwargs)
